39ca2948dbe6d081e877d52ed0e4b7939d2ef93b
bercucimihaela <bercucimihaela@gmail.com>
2026-06-15 23:37:17 +0300

    Create constants.py

---
+++ logic/constants.py
@@ -0,0 +1,16 @@
+COLOR_GRID = "#4b2c5e"
+COLOR_EMPTY = "#6d4685"
+
+COLOR_CELLS = {
+    2: ("#ffcce7", "#7d2e52"),
+    4: ("#ff99d1", "#7d2e52"),
+    8: ("#ff66bb", "#ffffff"),
+    16: ("#e066ff", "#ffffff"),
+    32: ("#b266ff", "#ffffff"),
+    64: ("#8000ff", "#ffffff"),
+    128: ("#66ffb2", "#1d5e3d"),
+    256: ("#33ff99", "#1d5e3d"),
+    512: ("#00ff80", "#1d5e3d"),
+    1024: ("#66ffff", "#1e5e5e"),
+    2048: ("#00e5ff", "#1e5e5e"),
+}
